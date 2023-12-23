ac01ccdce76e60cde7b3bf562c7fbc91d26e4116
Saroj Pedada <sarojpedada@gmail.com>
2023-12-23 19:29:26 +0530

    second main commit

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-var canvas = document.getElementById("canvas")
-var ctx = canvas.getContext("2d")
-ctx.fillStyle = "#000"
-ctx.fillRect(0, 0, 300, 300)
+const form = document.getElementById("new_document_attachment");
+const fileInput = document.getElementById("document_attachment_doc");
+
+fileInput.addEventListener("change", () => {
+  form.submit();
+});
+
+window.addEventListener("paste", (e) => {
+  fileInput.files = e.clipboardData.files;
+});
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
