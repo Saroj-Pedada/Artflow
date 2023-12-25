39eb46edcfa4e273fac13ec0ccd903579af5f98f
Saroj Pedada <sarojpedada@gmail.com>
2023-12-25 11:04:26 +0530

    Major Changes Pt.5

--- index.js
+++ index.js
@@ -10,26 +10,10 @@ window.addEventListener("paste", (e) => {
     f(e.clipboardData.files[0]);
 });
 
-document.addEventListener("wheel", function (e) {
-    if (e.deltaY > 0) {
-        zoom -= ZOOM_SPEED;
-        let images = document.getElementsByTagName("img");
-        for (var image of images) {
-            image.style.transform = `scale(${(zoom)},${(zoom)})`;
-        }
-    } else {
-        zoom += ZOOM_SPEED;
-        let images = document.getElementsByTagName("img")
-        for (var image of images) {
-            image.style.transform = `scale(${(zoom)},${(zoom)})`;
-        }
-    }
-});
-
 function dragAndResize() {
     $(".item").draggable();
     $(".item").resizable();
-};
+}
 
 function f(file) {
     id = id + 1;
@@ -38,6 +22,7 @@ function f(file) {
         var imageBox = document.createElement("div");
         imageBox.classList.add("item");
         imageBox.id = String(id);
+        imageBox.addEventListener("wheel", (k) => s(k, imageBox));
         var image = document.createElement("img");
         image.src = e.target.result;
         image.id = String(id);
@@ -47,4 +32,14 @@ function f(file) {
     };
     reader.readAsDataURL(file);
     console.log(id);
+}
+
+function s(e, div) {
+    if (e.deltaY > 0) {
+        zoom -= ZOOM_SPEED;
+        div.style.transform = `scale(${zoom},${zoom})`;
+    } else {
+        zoom += ZOOM_SPEED;
+        div.style.transform = `scale(${zoom},${zoom})`;
+    };
 }
