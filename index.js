8ef80385d8b73fbe38e6007f2b929d07bd881ef8
Saroj Pedada <sarojpedada@gmail.com>
2023-12-25 13:31:48 +0530

    Major Update Pt.6

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ var id = document.getElementsByTagName("img").length;
 var zoom = 1;
 const ZOOM_SPEED = 0.05;
 
-function myFunction() {
-    f(document.getElementById("file").files[0]);
-}
-
 window.addEventListener("paste", (e) => {
     f(e.clipboardData.files[0]);
 });
+
+function dblclicker(id) {
+    let z = Number(document.getElementById(id).style.zIndex);
+    console.log(Number(z))
+    document.getElementById(id).style.zIndex = z + 1;
+    console.log(id + "-" + z);
+}
+
+function myFunction() {
+    f(document.getElementById("file").files[0]);
+}
 
 function dragAndResize() {
     $(".item").draggable();
@@ -19,13 +26,14 @@ function f(file) {
     id = id + 1;
     var reader = new FileReader();
     reader.onload = function (e) {
+        let temp = id;
         var imageBox = document.createElement("div");
         imageBox.classList.add("item");
         imageBox.id = String(id);
         imageBox.addEventListener("wheel", (k) => s(k, imageBox));
+        imageBox.addEventListener("dblclick", () => dblclicker(temp));
         var image = document.createElement("img");
         image.src = e.target.result;
-        image.id = String(id);
         imageBox.appendChild(image);
         document.body.appendChild(imageBox);
         dragAndResize();
@@ -35,11 +43,11 @@ function f(file) {
 }
 
 function s(e, div) {
-    if (e.deltaY > 0) {
+    if (e.deltaY > 0 && zoom >= 0.6) {
         zoom -= ZOOM_SPEED;
         div.style.transform = `scale(${zoom},${zoom})`;
-    } else {
+    } else if (e.deltaY < 0 && zoom <= 1.4) {
         zoom += ZOOM_SPEED;
         div.style.transform = `scale(${zoom},${zoom})`;
-    };
+    }
 }
