676c3ee571052c2b646fa48055eeef7359c08040
Saroj Pedada <sarojpedada@gmail.com>
2024-01-13 23:19:35 +0530

    Imp Update

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-var id = document.getElementsByTagName("img").length;
-var zoom = 1;
-const ZOOM_SPEED = 0.05;
-
 window.addEventListener("paste", (e) => {
     f(e.clipboardData.files[0]);
 });
-
-function dblclicker(id) {
-    let z = Number(document.getElementById(id).style.zIndex);
-    document.getElementById(id).style.zIndex = z + 1;
-}
 
 function myFunction() {
     f(document.getElementById("file").files[0]);
@@ -21,27 +12,32 @@ function dragAndResize() {
 }
 
 function f(file) {
-    id = id + 1;
     var reader = new FileReader();
     reader.onload = function (e) {
-        let temp = id;
         var cross = document.createElement("div");
-        var imageBox = document.createElement("div");
-        imageBox.classList.add("item");
-        imageBox.id = String(id);
-        imageBox.addEventListener("dblclick", () => dblclicker(temp));
-        var image = document.createElement("img");
-        image.src = e.target.result;
         cross.innerHTML = "&times;";
         cross.classList.add("cross");
         cross.addEventListener("click", () => {
             document.body.removeChild(imageBox);
         });
+        var image = document.createElement("img");
+        image.src = e.target.result;
+        var imageBox = document.createElement("div");
+        imageBox.classList.add("item");
+        imageBox.addEventListener("dblclick", () => {
+            imageBox.style.zIndex = Number(imageBox.style.zIndex) + 1;
+        });
+        imageBox.setAttribute('tabindex', '0');
+        imageBox.addEventListener("keydown", (e) => {
+            if (e.key == "Backspace") {
+                e.preventDefault();
+                document.body.removeChild(imageBox);
+            }
+        })
         imageBox.appendChild(cross);
         imageBox.appendChild(image);
         document.body.appendChild(imageBox);
         dragAndResize();
     };
     reader.readAsDataURL(file);
-    console.log(id);
 }
